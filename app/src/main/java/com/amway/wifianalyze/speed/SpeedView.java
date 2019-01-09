47307a5bbce6c2044f5c0174d2e67cfef51ecca9
chenjianai <jianaichen@gmail.com>
2019-01-09 10:07:45 +0800

    更改检测项速度高度。

--- app/src/main/java/com/amway/wifianalyze/speed/SpeedView.java
+++ app/src/main/java/com/amway/wifianalyze/speed/SpeedView.java
@@ -1,12 +1,14 @@
 package com.amway.wifianalyze.speed;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.Rect;
 import android.graphics.RectF;
 import android.util.AttributeSet;
 
+import com.amway.wifianalyze.R;
 import com.autofit.widget.ScreenParameter;
 import com.autofit.widget.View;
 
@@ -26,15 +28,25 @@ public class SpeedView extends View {
     private static final int COLOR_BLUE = 0xff002d66;
     private static final int COLOR_RED = 0xffe70241;
     private static final int COLOR_LINE = 0x3ee70241;
+    private int mDotColor = COLOR_BLUE;
+    private int mDotColorSelect = COLOR_RED;
+    private int mLineColor = COLOR_LINE;
+    private int mLineColorSelected = COLOR_RED;
     private String[] tags = new String[]{"自行车", "汽车", "飞机", "火箭"};
 
     public SpeedView(Context context, AttributeSet attrs) {
         super(context, attrs);
         radius = ScreenParameter.getFitWidth(this, radius);
-//        lineLength = ScreenParameter.getFitWidth(this, lineLength);
         margingX = ScreenParameter.getFitWidth(this, margingX);
         margingY = ScreenParameter.getFitHeight(this, margingY);
         mPaint.setTextSize(ScreenParameter.getFitSize(this, textSize));
+        mPaint.setStrokeWidth(ScreenParameter.getFitHeight(this, 2));
+        final TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.SpeedView, 0, 0);
+        mDotColor = a.getColor(R.styleable.SpeedView_dotColor, COLOR_BLUE);
+        mDotColorSelect = a.getColor(R.styleable.SpeedView_dotColorSelected, COLOR_RED);
+        mLineColor = a.getColor(R.styleable.SpeedView_lineColor, COLOR_LINE);
+        mLineColorSelected = a.getColor(R.styleable.SpeedView_lineColorSelected, COLOR_RED);
+        a.recycle();
     }
 
 
@@ -47,13 +59,13 @@ public class SpeedView extends View {
         lineLength = (getWidth() - radius * 2 * 4 - margingX * 2) / 3;
         int x = margingX;
         for (int i = 0; i < 4; i++) {
-            mPaint.setColor(i == level ? COLOR_RED : COLOR_BLUE);
+            mPaint.setColor(i == level ? mDotColorSelect : mDotColor);
             RectF rect = new RectF(x, height - radius, x + 2 * radius, height + radius);
             canvas.drawOval(rect, mPaint);
             mPaint.getTextBounds(tags[i], 0, tags[i].length(), mBounds);
             canvas.drawText(tags[i], x + radius - mBounds.width() / 2, height + margingY, mPaint);
             if (i < 3) {
-                mPaint.setColor(i < level ? COLOR_RED : COLOR_LINE);
+                mPaint.setColor(i < level ? mLineColorSelected : mLineColor);
                 x += radius * 2;
                 canvas.drawLine(x, height, x + lineLength, height, mPaint);
                 x += lineLength;

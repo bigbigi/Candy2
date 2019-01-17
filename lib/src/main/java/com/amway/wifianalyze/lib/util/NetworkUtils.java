f5f4f0150c78e450f14dd054b9e51227eabb67ea
chenjianai <jianaichen@gmail.com>
2019-01-17 10:23:32 +0800

    添加7.0mac获取。

--- lib/src/main/java/com/amway/wifianalyze/lib/util/NetworkUtils.java
+++ lib/src/main/java/com/amway/wifianalyze/lib/util/NetworkUtils.java
@@ -18,8 +18,10 @@ import java.io.LineNumberReader;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.net.NetworkInterface;
 import java.net.SocketException;
 import java.net.UnknownHostException;
+import java.util.Enumeration;
 
 /**
  * Created by big on 2018/10/18.
@@ -122,8 +124,11 @@ public class NetworkUtils {
     public static String getMac(Context context) {
         Log.e(TAG, "VERSION:" + Build.VERSION.SDK_INT);
         String mac = getWlanMac();
-        if (TextUtils.isEmpty(mac)) {
+        if (TextUtils.isEmpty(mac) || "02:00:00:00:00:00".equals(mac.trim())) {
             mac = getWifiInfoMac(context);
+        }
+        if (TextUtils.isEmpty(mac) || "02:00:00:00:00:00".equals(mac.trim())) {
+            mac = getLocalMacAddressFromIp();
         }
         return mac;
     }
@@ -163,6 +168,65 @@ public class NetworkUtils {
         }
 
         return mac;
+    }
+
+    /**
+     * 根据IP地址获取MAC地址
+     *
+     * @return
+     */
+    private static String getLocalMacAddressFromIp() {
+        String strMacAddr = null;
+        try {
+            //获得IpD地址
+            InetAddress ip = getLocalInetAddress();
+            byte[] b = NetworkInterface.getByInetAddress(ip).getHardwareAddress();
+            StringBuffer buffer = new StringBuffer();
+            for (int i = 0; i < b.length; i++) {
+                if (i != 0) {
+                    buffer.append(':');
+                }
+                String str = Integer.toHexString(b[i] & 0xFF);
+                buffer.append(str.length() == 1 ? 0 + str : str);
+            }
+            strMacAddr = buffer.toString().toUpperCase();
+        } catch (Exception e) {
+
+        }
+
+        return strMacAddr;
+    }
+
+    /**
+     * 获取移动设备本地IP
+     *
+     * @return
+     */
+    private static InetAddress getLocalInetAddress() {
+        InetAddress ip = null;
+        try {
+            //列举
+            Enumeration<NetworkInterface> en_netInterface = NetworkInterface.getNetworkInterfaces();
+            while (en_netInterface.hasMoreElements()) {//是否还有元素
+                NetworkInterface ni = (NetworkInterface) en_netInterface.nextElement();//得到下一个元素
+                Enumeration<InetAddress> en_ip = ni.getInetAddresses();//得到一个ip地址的列举
+                while (en_ip.hasMoreElements()) {
+                    ip = en_ip.nextElement();
+                    if (!ip.isLoopbackAddress() && ip.getHostAddress().indexOf(":") == -1)
+                        break;
+                    else
+                        ip = null;
+                }
+
+                if (ip != null) {
+                    break;
+                }
+            }
+        } catch (SocketException e) {
+
+            e.printStackTrace();
+        }
+        return ip;
     }
 
     public static String getPhoneNumber(Context context) {
